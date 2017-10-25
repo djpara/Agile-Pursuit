c0b3489bd08d6fe129194d295163ebc19d4cb28a
David <davidjpara@gmail.com>
2017-10-25 01:29:24 -0500

    complete restructure - some bugs

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,12 @@
 package main;
 
 import gameObjects.GameBoard;
-import gameObjects.GridSpace;
 
 public class Main {
 
 	public static void main(String[] args) {
 		GameBoard gameBoard = GameBoard.getGameBoardInstance();
-		GridSpace gridspace = new GridSpace(10, 10);
+		gameBoard.init();
 	}
 
 }
