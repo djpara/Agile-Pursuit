52ca4781815e12bdad340e739cf1fc9906918048
David <davidjpara@gmail.com>
2017-11-11 12:51:38 -0600

    added JUnit5 framework

---
+++ src/test/panels/PlaySpacePanelTest.java
@@ -0,0 +1,24 @@
+package test.panels;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class PlaySpacePanelTest {
+
+    @BeforeAll
+    public void setUp() {
+
+    }
+
+    @AfterAll
+    public void tearDown() {
+
+    }
+
+    @Test
+    public void doSomething() {
+        
+    }
+
+}
